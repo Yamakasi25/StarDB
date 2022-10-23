9346478a8b76e2606f6fe8af931aa413136bf574
Yamakasi25 <runilyasrun@mail.ru>
2022-10-23 16:49:43 +0300

    State commit

--- src/components/random-planet/index.js
+++ src/components/random-planet/index.js
@@ -20,6 +20,10 @@ export default class RandomPlanet extends Component {
 		this.interval = setInterval(this.updatePlanet, 5000)
 	}
 
+	componentWillUnmount() {
+		clearInterval(this.interval)
+	}
+
 	onPlanetLoadet = planet => {
 		this.setState({ planet, loading: false })
 	}
